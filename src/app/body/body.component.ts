4419b003721db2b56fea76cd044072cec86bb241
ostap <oneostap@gmail.com>
2018-01-22 09:25:23 +0200

    add more templates

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -9,6 +9,13 @@ export class BodyComponent implements OnInit {
 
   public shares_path: string = 'assets/img/shares.jpg';
   public product_path: string = 'assets/img/havka.jpg';
+  public shares_2_path: string = 'assets/img/shares_image.jpg';
+
+  public document_image: string = 'assets/img/document.jpg';
+
+  public how_to_icon: string = 'assets/img/howToIcon.png';
+
+  public logo_path: string = '/assets/logo/Yakudza.svg';
 
   constructor() { }
 
